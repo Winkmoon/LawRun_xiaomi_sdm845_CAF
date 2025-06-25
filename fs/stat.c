393594d29374d609fd06aadb122e71dd813bdc66
Mohammed Ahmed <mohammedaelnaggar1@gmail.com>
2025-06-25 19:33:11 +0200

    KernelSU: Fs: Stats: Fix compilation

--- fs/stat.c
+++ fs/stat.c
@@ -97,9 +97,6 @@ int vfs_fstatat(int dfd, const char __user *filename, struct kstat *stat,
 	struct path path;
 	int error = -EINVAL;
 	unsigned int lookup_flags = 0;
-#ifdef CONFIG_KSU 
-	ksu_handle_stat(&dfd, &filename, &flag);
-#endif
 
 	if ((flag & ~(AT_SYMLINK_NOFOLLOW | AT_NO_AUTOMOUNT |
 		      AT_EMPTY_PATH)) != 0)
@@ -300,6 +297,10 @@ SYSCALL_DEFINE4(newfstatat, int, dfd, const char __user *, filename,
 {
 	struct kstat stat;
 	int error;
+
+#ifdef CONFIG_KSU
+	ksu_handle_stat(&dfd, &filename, &flag);
+#endif
 
 	error = vfs_fstatat(dfd, filename, &stat, flag);
 	if (error)
